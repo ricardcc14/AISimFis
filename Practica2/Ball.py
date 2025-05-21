98a91a98b90ba37d19e0673c79fcc04c2d5dbdde
alexaloonso <alex.aa@students.salle.url.edu>
2025-05-21 16:11:13 +0200

    Practica 2

---
+++ Practica2/Ball.py
@@ -0,0 +1,37 @@
+import pygame
+import Box2D as b2
+import utils
+
+class Ball:
+    def __init__(self, world:b2.b2World, x:float, y:float, radius:float):
+        self.radius = radius
+        self.isRemoved = False
+        
+        bodydf = b2.b2BodyDef()
+        bodydf.position = utils.pixelToWorld(x, y)
+        bodydf.type = b2.b2_dynamicBody
+        bodydf.userData = self
+        self.body:b2.b2Body = world.CreateBody(bodydf)
+
+        circleshape = b2.b2CircleShape(radius=utils.pixelToWorld(radius))
+        fd = b2.b2FixtureDef()
+        fd.shape=circleshape
+        fd.density = 1
+        fd.restitution = 0
+        fd.friction = 1
+        self.body.CreateFixture(fd)
+        pass
+
+    def draw(self, screen:pygame.Surface):
+        position:b2.b2Vec2 = utils.worldToPixel(self.body.position.copy())
+        position.y = screen.get_height()-position.y
+
+        pygame.draw.circle(screen, 'blue', position, self.radius, 1)
+        pass
+
+    def update(self, world):
+        pass
+
+    def destroy(self, world:b2.b2World):
+        world.DestroyBody(self.body)
+        self.isRemoved = True
---
+++ Practica2/main.py
@@ -0,0 +1,32 @@
+#Libraries
+import pygame
+import Box2D as b2
+import utils
+import time
+
+from Ball import Ball
+from EvolutionManager import EvolutionManager
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((640, 400))
+clock = pygame.time.Clock()
+frames = 60
+running = True
+
+#Controllers
+evolutionManager = EvolutionManager(screen, 'data.json')
+evolutionManager.loadElements()
+
+while running:
+    screen.fill('gray')
+    evolutionManager.draw()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+    pygame.display.flip()
+    clock.tick(frames)
+
+pygame.quit()
